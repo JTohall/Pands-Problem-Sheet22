120bb0e5e15015c7c87f330e31357892f304a66a
Jamie <jamietohall@hotmail.com>
2022-02-07 21:18:13 +0000

    Week 3 task

---
+++ 3.SecondString.py
@@ -0,0 +1,7 @@
+# Programme will ask the user to input a string and outputs every second letter in reverse order
+#  Author: Jamie Tohall
+
+Sentence = input("Please enter a sentence:")
+
+print(Sentence[::-2])
+
