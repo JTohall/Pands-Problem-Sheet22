61350d25c86d4d54915417b0bba13a1ab0ef5bae
Jamie <jamietohall@hotmail.com>
2022-03-21 23:23:16 +0000

    Week 6 task

---
+++ 6.Squareroot.py
@@ -0,0 +1,24 @@
+# Write a programme that takes a postive floating point number and outputs an approximation
+# of its square root
+#Author: Jamie Tohall
+
+Number = int (input("Please enter a positive number:"))
+ 
+#define positive number
+#if X %2:
+    #print(result)
+    
+#elif X <0:
+    #print("Cannot accept a negative number. Please enter a positive number")
+    
+#elif X 
+
+#defining a negative number so I can return an error
+
+
+# Defining my function 
+result = Number**0.5
+        
+#results-
+print ("The square root of {} is approx {} ".format(Number, result))
+ 
