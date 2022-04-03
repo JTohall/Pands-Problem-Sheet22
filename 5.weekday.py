f9bd0bc81914074b2c47bbcb81463061d5b2e159
Jamie <jamietohall@hotmail.com>
2022-04-03 20:28:20 +0100

    week 5 task - Weekday or weekend

--- 5.weekday.py
+++ 5.weekday.py
@@ -1,4 +1,4 @@
-# Programme will output what day of the week it is
+# Programme will let the user know if its a weekday or the weekend 
 # Author: Jamie Tohall
 
 # Import datetime module
@@ -7,17 +7,9 @@ import datetime
 # Define day
 day = datetime.datetime.today().weekday()
 
-# Each weekday in python datetime is represented by a number, Monday = 0, 
-# Tuesday = 1 etc. 
-
-if day <5: print("Unfortunately today as a weekday :(")
+# Each weekday in python datetime is represented by a number, Monday = 0, Tuesday = 1 etc. 
 # Therefore if the day is less than 5 (Saturday) It will be a weekday.
+if day <5: print("Unfortunately today is a weekday :(")
 
 else: print("It's the weekend, yay!")
-# other two options will be Saturday and Sunday, or 5 & 6, so we know it will be the weekend.
-
-
-
-
-
-
+# Other two options will be Saturday and Sunday, or 5 & 6, so we know it will be the weekend.
