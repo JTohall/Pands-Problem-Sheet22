7ea2610dbc702a142fbb9a9ac8dd20f71b9bbc7f
Jamie <jamietohall@hotmail.com>
2022-04-03 20:58:26 +0100

    Week 7 task - Es in Moby Dick

--- 7.Es.py
+++ 7.Es.py
@@ -3,12 +3,10 @@
 
 
 import sys
-
 # Import the sys module 
 
 
 filename = sys.argv[1]
-
 # Sys.argv is a command in python that contains all the command-line arguments passed to the script 
 # Sys.argv[1] refers to the second argument in the command line, in this case the second argument 
 # is 'MobyDick.txt'
@@ -18,23 +16,19 @@ filename = sys.argv[1]
 
 
 with open (filename, "rt") as f: 
-    #"rt" will open the MobyDick.txt file in read and text mode, and saved the file as 'f', 
-    # meaning that 'f' now represents the text file in this programme
+#"rt" will open the MobyDick.txt file in read and text mode, and saved the file as 'f', 
+# meaning that 'f' now represents the text file in this programme
     
         data = f.read()
         e = data.count ("e")
         E = data.count ("E") 
         
-    # data.count will return the number of elements with the specified value, in this case the 
-    # value is the letter E in both upper and lower case
-# I assume that the count method does not count both upper and lower case letters together so
-# I counted them both seperately, then I will add them together for the output number
+# data.count will return the number of elements with the specified value, in this case the 
+# value is the letter E in both upper and lower case.
+# I assume that the count method does not count both upper and lower case letters together so I 
+# counted them both seperately, then I will add them together for the output.
         
 print("There are a total of {} letter E's in this file".format(e + E))
 # To clarify I used an upper case E in the output sentence, however I'm including all upper
-# and lower case E's contained in the text file.
-
-
-
-    
-    
+# and lower case E's contained in the text file. I added the amount of upper and lower case
+# E's together in the format section '(E + e)' to get the final number of 150.
