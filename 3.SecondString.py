20ded579ee945f80066e3ecce2e528290c123ff3
Jamie <jamietohall@hotmail.com>
2022-04-03 20:19:40 +0100

    Week 3 task - Second String

--- 3.SecondString.py
+++ 3.SecondString.py
@@ -1,7 +1,9 @@
 # Programme will ask the user to input a string and outputs every second letter in reverse order
-#  Author: Jamie Tohall
+# Author: Jamie Tohall
 
 Sentence = input("Please enter a sentence:")
+# Will ask the user to input a sentence, define the sentence as 'Sentence'.
 
 print(Sentence[::-2])
-
+# Using the slicing syntax [::-2] the sentence will be reversed and every second letter will be printed. 
+# If I were to do [::-1] the whole sentence will be printed in reverse order without missing words.
